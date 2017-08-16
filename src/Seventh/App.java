f11e480f61e2039cbe07e97d6c75bf163788d732
binio <tbiniecki@gmail.com>
2017-08-16 11:03:36 +0100

    BlockingQueue producer / consumer

--- src/Seventh/App.java
+++ src/Seventh/App.java
@@ -1,11 +1,66 @@
 package Seventh;
 
+import java.util.Random;
+import java.util.concurrent.ArrayBlockingQueue;
+import java.util.concurrent.BlockingDeque;
+import java.util.concurrent.BlockingQueue;
+
 /**
  * Created by tomasz.biniecki on 16/08/2017.
  */
 public class App {
 
-    public static void main(String[] args) {
+    private static BlockingQueue<Integer> queue = new ArrayBlockingQueue(10);
 
+    public static void main(String[] args) {
+        Thread t1 = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    producer();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+
+        Thread t2 = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    consumer();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+
+        t1.start();
+        t2.start();
+
+        try {
+            t1.join();
+            t2.join();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static void producer() throws InterruptedException{
+        Random random = new Random();
+        while(true) {
+            queue.put(random.nextInt(100));
+        }
+    }
+
+    private static void consumer() throws InterruptedException{
+        Random random = new Random();
+        while(true){
+            Thread.sleep(10);
+            if(random.nextInt(10) == 0){
+                Integer value = queue.take();
+                System.out.println("Taken value: "+ value + " Size is: "+ queue.size());
+            }
+        }
     }
 }
