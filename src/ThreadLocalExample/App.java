a9217ce9d5e295015b31e37e26fd72f9a86a9ed6
tomasz.biniecki <tomasz.biniecki@wowcher.co.uk>
2017-09-14 09:35:49 +0100

    some description

--- src/ThreadLocalExample/App.java
+++ src/ThreadLocalExample/App.java
@@ -24,8 +24,10 @@ public class App {
 
         /*
             full example
-            All threads share MyRunnable and through it its ThreadLocal
-            run() will change value of ThreadLocal
+            This example creates a single MyRunnable instance which is passed to two different threads. Both threads
+            execute the run() method, and thus sets different values on the ThreadLocal instance. If the access to the
+            set() call had been synchronized, and it had not been a ThreadLocal object, the second thread would have
+            overridden the value set by the first thread.
          */
 
         MyRunnable sharedRunnableInstance = new MyRunnable();
